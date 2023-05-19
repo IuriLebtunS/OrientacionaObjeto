58c43bbe9b11df628396fcdaf80760fef204241f
IuriLebtunS <lebtuniuri@gmail.com>
2023-05-19 16:49:10 -0400

    Estaticos

---
+++ 12-Estaticos/Exemplo.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+class Exemplo
+{
+   public static string nome = "Iuri Lebtun";
+   public static void soma(int n1, int n2)
+   {
+     Console.WriteLine( n1 + n2 );
+   }
+
+}
--- 12-Estaticos/Program.cs
+++ 12-Estaticos/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace _12Estaticos
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Exemplo.soma( 3 , 7 );
+
+            Console.WriteLine(" Ola " + Exemplo.nome);
+        }
+    }
+}
